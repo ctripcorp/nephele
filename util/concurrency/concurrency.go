222b3e4de68d8052b3e39c28f422c4b4126bc0af
phyxdown <264162213@qq.com>
2018-03-28 16:03:16 +0800

    concurrency limiter

--- util/concurrency/concurrency.go
+++ util/concurrency/concurrency.go
@@ -1,24 +1,56 @@
 package concurrency
 
 import (
+	"errors"
+	"fmt"
+	"sync"
 	"time"
 )
 
 var limiters map[string]*limiter
+var limiterMutex sync.RWMutex
 
-type limiter struct {
-	p *pool
+func init() {
+	limiters = make(map[string]*limiter)
 }
 
-func (l *limiter) Do() error {
-	_, err := l.p.get()
-	return err
+type limiter struct {
+	concurrency *pool
+	wait        *pool
+}
+
+func (l *limiter) Do(timeout time.Duration) error {
+	if l.wait.get(0) != nil {
+		return errors.New("fast fail")
+	}
+	defer l.wait.put()
+	return l.concurrency.get(timeout)
 }
 
 func (l *limiter) Done() error {
-	return l.p.put(&poolItem{})
+	return l.concurrency.put()
 }
 
-func Limiter(name string, maxConcurrency int, maxWait int, timeout time.Time) *limiter {
-	return nil
+func Limiter(name string, maxConcurrency int, maxWait int) *limiter {
+	id := fmt.Sprintf("%s-%d-%d", name, maxConcurrency, maxWait)
+	limiterMutex.RLock()
+	l, ok := limiters[id]
+	limiterMutex.RUnlock()
+	if !ok {
+		limiterMutex.Lock()
+		c := &pool{}
+		c.init(maxConcurrency)
+		w := &pool{}
+		w.init(maxWait)
+		limiters[id] = &limiter{
+			concurrency: c,
+			wait:        w,
+		}
+		limiterMutex.Unlock()
+
+		limiterMutex.RLock()
+		l = limiters[id]
+		limiterMutex.RUnlock()
+	}
+	return l
 }
--- util/concurrency/pool.go
+++ util/concurrency/pool.go
@@ -8,37 +8,34 @@ import (
 type poolItem struct{}
 
 type pool struct {
-	timeout time.Duration
-
 	items chan *poolItem
 }
 
-func (p *pool) init(timeout time.Duration, max int) {
-	p.timeout = timeout
+func (p *pool) init(max int) {
 	p.items = make(chan *poolItem, max)
 	for i := 0; i < max; i++ {
 		p.items <- &poolItem{}
 	}
 }
 
-func (p *pool) get() (*poolItem, error) {
-	items := p.items
-	if items == nil {
-		return nil, errors.New("pool is closed")
-	}
-	select {
-	case item := <-items:
-		return item, nil
-	case <-time.After(p.timeout):
-		return nil, errors.New("timeout")
-	}
-}
-
-func (p *pool) put(item *poolItem) error {
+func (p *pool) get(timeout time.Duration) error {
 	items := p.items
 	if items == nil {
 		return errors.New("pool is closed")
 	}
-	items <- item
+	select {
+	case <-items:
+		return nil
+	case <-time.After(timeout):
+		return errors.New("timeout")
+	}
+}
+
+func (p *pool) put() error {
+	items := p.items
+	if items == nil {
+		return errors.New("pool is closed")
+	}
+	items <- &poolItem{}
 	return nil
 }
